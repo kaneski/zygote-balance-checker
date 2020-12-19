be72f8a952661ec4e0313d4051a26b4510f4332b
kaneski <kaneskee@gmail.com>
2020-12-20 00:37:38 +0800

    extend get_balance script

---
+++ get_balance_all_lt_50.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+modem_index=$1
+database=$2
+
+echo "mobile_number,balance,valid_until,asset_code"
+for line in $(sqlite3 $database "SELECT mobile_number, asset_code FROM devices"); do
+    mobile_number=$(echo $line | cut -d"|" -f 1)
+    asset_code=$(echo $line | cut -d"|" -f 2)
+    result=$(./get_balance_lt_50.sh $modem_index $mobile_number)
+    echo "$result,$asset_code"
+done 
