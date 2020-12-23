119b257658813a0661e6ac139b4ba624fdc83d11
kaneski <kaneskee@gmail.com>
2020-12-23 22:03:52 +0800

    silence ussd-cancel warning

--- get_balance_lt_50.sh
+++ get_balance_lt_50.sh
@@ -4,7 +4,7 @@ mobile_number=$2
 ussd_number="*143#"
 balance_threshold=50
 
-mmcli -m "$modem_index" --3gpp-ussd-cancel 1>/dev/null
+mmcli -m "$modem_index" --3gpp-ussd-cancel 1>/dev/null 2>&1
 mmcli -m "$modem_index" --3gpp-ussd-initiate="$ussd_number" 1>/dev/null
 mmcli -m "$modem_index" --3gpp-ussd-respond="0" 1>/dev/null
 mmcli -m "$modem_index" --3gpp-ussd-respond="5" 1>/dev/null
@@ -14,7 +14,7 @@ mmcli -m "$modem_index" --3gpp-ussd-respond="$mobile_number" 1>/dev/null
 final_response=$(mmcli -m "$modem_index" --3gpp-ussd-respond="1")
 float_balance=$(echo $final_response | cut -d" " -f 20)
 valid_until=$(echo $final_response | cut -d" " -f 23)
-mmcli -m "$modem_index" --3gpp-ussd-cancel 1>/dev/null
+mmcli -m "$modem_index" --3gpp-ussd-cancel 1>/dev/null 2>&1
 
 balance=${float_balance:1}
 balance=$(echo $balance | cut -d"." -f 1)
